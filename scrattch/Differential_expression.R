d341a89d690064f2b6e654e344912ca759209587
Xiaoping Liu <xiaoping.liu@xiaopingl-ux1.corp.alleninstitute.org>
2023-10-25 11:35:32 -0700

    Grid violin plot and first IPFX trace pulling

--- scrattch/Differential_expression.R
+++ scrattch/Differential_expression.R
@@ -4,10 +4,18 @@ mappingFolder <- "/home/xiaoping.liu/scrattch/mapping/NHP_BG_AIT_115/"
 #dir.create(mappingFolder, showWarnings=FALSE)
 data_dir = "/allen/programs/celltypes/workgroups/rnaseqanalysis/SMARTer/STAR/Macaque/patchseq/R_Object"
 
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+#if (!require("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+
+#BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
+#                       'limma', 'lme4', 'S4Vectors', 'SingleCellExperiment',
+#                       'SummarizedExperiment', 'batchelor', 'HDF5Array',
+#                       'terra', 'ggrastr'))
 
 BiocManager::install("EnhancedVolcano")
+
+#install.packages("devtools")
+#devtools::install_github('cole-trapnell-lab/monocle3')
 
 #browseVignettes("EnhancedVolcano")
 
@@ -35,7 +43,6 @@ goi = read.csv(file.path(mappingFolder,'VGIC_short.csv'))   # Genes of interest
 #pbmc <- LoadData("pbmc3k", type = "pbmc3k.final")
 #levels(pbmc)
 
-#AIT.anndata <- loadTaxossh -N -L 8787:n77:8787 xiaoping.liu@hpc-loginnomy(refFolder)
 # Use complete taxonomy
 #AIT.anndata <- read_h5ad(file.path(refFolder, "NHP_BG_AIT115_complete.h5ad"))
 AIT.anndata <- loadTaxonomy(refFolder)
@@ -100,7 +107,7 @@ deg_comp <- function(Expr.dat, annoBG, subclasses, goi, type1, type2, level, FCc
   ##sample_factor = 10
   ##keepinds = sample(which(keepinds), round(sum(keepinds)/sample_factor))
   #Expr.dat_norm <- Expr.dat_norm[,keepinds]   # Or Expr.dat if not normalized
-  #annoBG <- annoBG[keepinds,]
+  #annoBG <- annoBG[keepinds,]ssh -N -L 8787:n265:8787 xiaoping.liu@hpc-login
   #anno_type <- anno_type[keepinds]
   #anno_type_color <-anno_type_color[keepinds]
 
@@ -136,8 +143,9 @@ list('D1-Matrix', 'D2-Matrix', 'level3.subclass_label', 'D1-Matrix vs D2-Matrix'
 list(c("D1-Matrix", "D2-Matrix"), c("D1-Striosome", "D2-Striosome"), 'level3.subclass_label', 'Matrix vs Striosome'),
 list(c("D1-Matrix", "D2-Matrix"), c("D1-ShellOT", "D2-ShellOT"), 'level3.subclass_label', 'Dorsal vs Ventral'),
 list('D1D2-Hybrid', c("D1-Matrix", "D2-Matrix", "D1-Striosome", "D2-Striosome"), 'level3.subclass_label', 'D1D2-Hybrid vs other dorsal MSN'),
-list(c("LHX6-TAC3-PLPP4","TAC3-LHX8-PLPP4"), NULL, 'level3.subclass_label', 'TAC3-PLPP4 vs rest'))
-list("SST_Chodl", NULL, 'level3.subclass_label', 'SST Chodl vs rest')
+list(c("LHX6-TAC3-PLPP4","TAC3-LHX8-PLPP4"), NULL, 'level3.subclass_label', 'TAC3-PLPP4 vs rest'),
+list("SST_Chodl", NULL, 'level3.subclass_label', 'SST Chodl vs rest'),
+list("CHAT", NULL, 'level3.subclass_label', 'CHAT vs rest'))
 #comparisons = list(list('D1-Matrix', 'D2-Matrix', 'level3.subclass_label'))
 #Expr.dat <- AIT.anndata$X   # These are already log transformed
 #Expr.dat <- AIT.anndata$layers['UMIs']   # For complete taxonomy (but doesn't match anno)
@@ -156,9 +164,15 @@ for (comp in comparisons){
   #} else {
   #  title = paste0(paste0(comp[[1]], collapse=' and '), '_vs_', paste0(comp[[2]], collapse=' and '))
   #}
+  
   title = comp[[4]]
+  if (title == 'CHAT vs rest') {
+    caption = 'FC cutoff, 0.5; p-value-adj cutoff, 10e-4'
+  } else {
+    caption = ''
+  }
   write.csv(markers, file.path(mappingFolder, paste0('DEG/', title, "_ion_channels_norm.csv")))
-  png(file.path(mappingFolder, paste0('DEG/', title, "_ion_channels_norm.png")), width = 600, height = 500)
+  png(file.path(mappingFolder, paste0('DEG/', title, "_ion_channels_norm.png")), width = 600, height = 400)
   print(EnhancedVolcano(markers,
     lab = rownames(markers),
     x = 'avg_log2FC',
@@ -168,10 +182,10 @@ for (comp in comparisons){
   #  xlim = c(-5.5, 5.5),
   #  ylim = c(0, -log10(10e-12)),
     pointSize = 1.5,
-    labSize = 2.5,
+    labSize = 4,
     title = title,
     subtitle = 'Ion channel DEGs, normalized',
-    caption = 'FC cutoff, 0.5; p-value-adj cutoff, 10e-4',
+    caption = caption,
     legendPosition = "right",
     legendLabSize = 14,
     col = c('grey30', 'forestgreen', 'royalblue', 'red2'),
@@ -188,28 +202,55 @@ for (comp in comparisons){
 # NOTE png's can be slow to save
 allMarkers <- unique(allMarkers)
 
-type1 = 'D1-Matrix'
-type2 = 'D2-Matrix'
-keepinds = anno_type == type1 | anno_type == type2
-Expr.dat <- Expr.dat[,keepinds]
-Expr.dat <- t(Expr.dat)
+library(patchwork)
+subclass_short = c("D1-Matrix", "D2-Matrix", "D1-Striosome", "D2-Striosome", "D1-ShellOT",
+             "D2-ShellOT", "D1D2-Hybrid", "D2-Hybrid-MCHR2", "D1-NUDAP", 
+             "PVALB-COL19A1-ST18", "SST_Chodl", "CCK-FBXL7", "CHAT", "CCK-VIP-TAC3", 
+              "LHX6-TAC3-PLPP4", "TAC3-LHX8-PLPP4") 
+genes = c("SCN1A", "SCN9A", "SCN4B", "KCNQ5", "KCNAB1", "KCNB2", "KCNC2", 
+             "KCND2", "KCNH1", "KCNH7", "KCNJ3", "KCNK2", "KCNT2", "KCNIP4", "HCN1", 
+             "CACNA1A", "CACNA1D", "ANO10", "ANO3", "ANO4", "ANO6")
+genes = c("SCN9A", "SCN4B", "KCNQ5", "KCNAB1", "KCNC2", 
+             "KCND2", "KCNH1", "KCNJ3", "KCNK2", "KCNT2", "KCNIP4",
+             "CACNA2D3") 
+
+level = 'level3.subclass_label'
+anno_type = annoBG[[level]]
+anno_type_color = annoBG[[level %>% str_replace("label", "color")]]
+keepinds = is.element(anno_type, subclass_short)
+Expr.dat <- Expr.dat[keepinds,]  # May be other dimension if running from contiguous
+#Expr.dat <- t(Expr.dat)
 anno_type <- anno_type[keepinds]
 anno_type_color <-anno_type_color[keepinds]
 #genes = c("KCNC2", "KCNQ5", "KCNIP4", "CACNA2D3")
-genes = allMarkers
+
+#anno_type <- str_wrap(anno_type, width = 5)
 p = list()
 for (i in 1:length(genes)){
     gene = genes[i]
     #scale_fill_brewer()
-    df <- data.frame(expr = Expr.dat[,gene], subclass = anno_type)
+    df <- data.frame(expr = Expr.dat[,gene], subclass = anno_type, subclass_color = anno_type_color)
     df <- mutate(df, expr_log = log2(expr + 1)) 
-
+    df$subclass <- factor(df$subclass, levels = rev(subclass_short))
     # Basic violin plot
-    p[[i]] <- ggplot(df, aes(x=subclass, y=expr_log)) + geom_violin(bw = 0.3) + geom_boxplot(width=0.1) 
-    p[[i]] <- p[[i]] + ggtitle(gene)
-}    
-grid.arrange(p[[1]], p[[2]], p[[3]], p[[4]], ncol=4)
-do.call(grid.arrange,c(p, ncol=4))
+    #p[[i]] <- ggplot(df, aes(x=subclass, y=expr_log)) + geom_violin(bw = 0.3) + geom_boxplot(width=0.1) 
+    p[[i]] <- ggplot(df, aes(x=subclass, y=expr_log, fill = subclass_color)) + geom_violin(bw = 0.3, color = 'darkgray', show.legend= FALSE)
+    p[[i]] <- p[[i]] + ylab(gene) + xlab(NULL) + 
+    theme(axis.title.y = element_text(angle=0, size = 36), axis.title.x = element_text(size = 36), axis.text.y = element_text(angle = 0, size = 36)) +
+     scale_y_discrete(position = "right") + coord_flip()
+    if (i > 1) {
+      p[[i]] <- p[[i]] + theme(axis.text.y = element_blank(), axis.ticks.y=element_blank())
+    }
+    
+}   
+png(file.path(mappingFolder, "/DEG/violins.png"), width = 2550, height = 1200)
+p[[1]] | p[[2]] | p[[3]] | p[[4]] | p[[5]] | p[[6]] | p[[7]] | p[[8]] | p[[9]] | p[[10]] | p[[11]] | p[[12]]
+dev.off()
+#+ scale_x_discrete(limits = subclass_short) + scale_y_discrete(position = "top") 
+
+
+#grid.arrange(p[[1]], p[[2]], p[[3]], p[[4]], nrow=4)
+#do.call(grid.arrange,c(p, ncol=4))
 
 # PRETTY UP COLORS
 # GET DOCALL TO WORK
@@ -219,17 +260,16 @@ do.call(grid.arrange,c(p, ncol=4))
 library(monocle)
 
 #Expr.dat <- AIT.anndata$layers['counts']
-subclass_short = c("D1-Matrix", "D2-Striosome", "D2-Matrix", "D2-Hybrid-MCHR2", 
-             "D1D2-Hybrid", "LHX6-TAC3-PLPP4", "D1-Striosome",
-             "PVALB-COL19A1-ST18", "CCK-VIP-TAC3", "CCK-FBXL7", "SST_Chodl", 
-             "D2-ShellOT", "CHAT", "D1-ShellOT", "D1-NUDAP", "TAC3-LHX8-PLPP4") 
 
-Expr.sub <- Expr.dat[,allMarkers]
+Expr.sub <- Expr.dat[,Markers_short]
 Expr.sub <- Expr.sub[is.element(annoBG$level3.subclass_label,subclass_short),]
 Expr.sp <- t(as.matrix(Expr.sub))
+write.csv(Expr.sp, file.path(mappingFolder, 'DEG/Expr_short.csv'))
 Expr.sp <- as(Expr.sp, "sparseMatrix") 
+
 fd <- data.frame(gene_short_name=rownames(Expr.sp))
 annoBG <- as.data.frame(annoBG[is.element(annoBG$level3.subclass_label,subclass_short),])
+write.csv(Expr.sp, file.path(mappingFolder, 'DEG/Anno_short.csv'))
 rownames(annoBG) = colnames(Expr.sp) 
 rownames(fd) = rownames(Expr.sp)
 cds <- newCellDataSet(Expr.sp,
@@ -242,3 +282,34 @@ cds <- newCellDataSet(Expr.sp,
 plot_genes_violin(cds, min_expr=0.1)
 
 # group_cells_by=annoBG["level3.subclass_Tree"], 
+
+#Need Monocle3 - hard to install
+
+#########
+
+#Expr = read.csv("/Users/xiaoping.liu/celltypes/NHP_BG_anal/DEG/Expr_short.csv")
+#brain.data     <- Expr  # This is already subsetted for cells and
+#genes, should be features are rows, cells are columns
+#Anno = read.csv("/Users/xiaoping.liu/celltypes/NHP_BG_anal/DEG/Anno_short.csv")
+ # rows are cells, columns are metadata fields
+#brain.metadata <- data.frame(subclass = Anno$level3.subclass_label)
+#rownames(brain.metadata) <- colnames(brain.data)
+
+
+detach("package:scrattch.bigcat", unload = TRUE)   # detach all scrattch packages on right panel
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("multtest")
+
+source("https://z.umn.edu/archived-seurat")    # Get Seurat V2
+
+
+## Construct data set lists
+brain.data <- Expr.dat
+brain.metadata <- annoBG
+
+brain <- CreateSeuratObject(counts = brain.data, meta.data = brain.metadata)
+# Should you transform counts?
+Idents(brain) <- brain.metadata$level3.subclass_label
+VlnPlot(object = brain, features = 'counts', idents = subclass_short)
